28c05c249ace38e3691d2ae1fe3253d80298ed8c
Justin Mangan <jm0015052@otc.edu>
2018-02-28 20:06:04 -0600

    Making an initial commit from home

--- includes/db.php
+++ includes/db.php
@@ -1,4 +1,12 @@
 <?php
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$database = getenv('DB_DATABASE');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+
+$dsn = "mysql:host=$host;port=$port;dbname=$database";
+$conn = new PDO($dsn, $username, $password);
 
 // You might connect to your database here. However, don't
 // hard-code your database credentials here! Instead,
--- index.php
+++ index.php
@@ -7,11 +7,66 @@
 require_once('./includes/init.php');
 
 // Here you might connect to the database and show off some of your newest guitars.
+require_once('./includes/db.php');
+// get all categories
+$queryAllCategories = 'SELECT * FROM categories ORDER BY categoryID';
+$statement1 = $conn->prepare($queryAllCategories);
+$statement1->execute();
+$categories = $statement1->fetchAll();
+$statement1->closeCursor();
 
+var_dump($categories);
 ?>
 
-This is my super awesome Guitar Shop!
+<!--This is my super awesome Guitar Shop!
 
 - All Orders
 - Unshipped Orders
-- Products
+- Products-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <title>Guitars</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" type="text/css" href="main.css"/> -->
+</head>
+    <body>
+        <div class="container">
+            <div class="text-center">
+                <h1>Guitars</h1>
+            </div>
+            
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <!--<h3>Categories</h3>-->
+            <h4 class="navbar-brand">Categories</h4>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">All Orders</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Unshipped Orders</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Products</a>
+                    </li>
+                </ul>
+            </div>
+            </nav>                 
+        </div>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
+    </body>
+</html>
