ba58c4bffe2a230bc1e0f926c748d71c08ac191b
Justin Mangan <jm0015052@otc.edu>
2018-03-04 22:35:09 -0600

    Working out a bug

--- pages/all_products.php
+++ pages/all_products.php
@@ -14,51 +14,48 @@ $title = "Guitars- all products";
 $categories = getMany("SELECT * FROM categories", [], $conn);
 //$products = getMany("SELECT * FROM products", [], $conn);
 
-//// get all products
-// $queryAllProducts = 'SELECT * FROM products ORDER BY productID';
-// $statement1 = $conn->prepare($queryAllProducts);
-// $statement1->execute();
-// $products = $statement1->fetchAll();
-// $statement1->closeCursor();
-
-//var_dump($products);
 ?>
 
-                    <!-- Header/Nav -->
+                    
                     <?php include("../includes/header_nav.php") ?>
 
-                    <!-- Unique page content here -->
-                    <div class="row align-items-center" style="height: 100%;">
-        <div class="col-sm"></div>
-        <div class="col-sm-6">
-            <div class="card text-white bg-dark mb-3">
-                <div class="card-header text-center font-weight-bold text-white bg-info mb-3">
-                    <h2>Products</h2>
-                </div>
-                <div class="card-body">
-                    <div>
-                    <?php foreach ($categories as $category) : ?>
-                        <h5><?php echo $category['categoryName']; ?></h5>
-                        <?php $currentCategory = $category['categoryID']; ?> 
-                        <?php $currentID = getMany("SELECT * FROM  products WHERE categoryID = $currentCategory", [], $conn); ?>
-                        <table class="table table-striped table-dark">
-                            <tr>
-                                <th scope="col">Name</th>
-                                <th scope="col">Price</th>
-                            </tr>
-                            <?php foreach ($currentID as $product) : ?>
-                            <tr>
-                                <td><a href="#"><?php echo $product['productName']; ?></a></td>
-                                <td>$<?php echo $product['listPrice']; ?></td>
-                            </tr>
-                            <?php endforeach; ?>
-                        <?php endforeach; ?>
-                        </table>
+                    <!--***Unique page content here*************************-->
+            <div class="row align-items-center" style="height: 100%;">
+                    <div class="col-sm"></div>
+                    <div class="col-sm-6">
+                        <div class="card text-white bg-dark mb-3">
+                            <div class="card-header text-center font-weight-bold text-white bg-info mb-3">
+                                <h2>Products</h2>
+                            </div>
+                            <div class="card-body">
+                                <div>
+                                <?php foreach ($categories as $category) : ?>
+                                <!---->
+                                    <h5 class="text-center"><?php echo $category['categoryName']; ?></h5>
+                                    <?php $currentCategory = $category['categoryID']; ?> 
+                                    <?php $currentID = getMany("SELECT * FROM  products WHERE categoryID = $currentCategory", [], $conn); ?>
+                                <!---->
+                                    <table class="table table-striped table-dark">
+                                        <tr>
+                                            <th scope="col">Product Code</th>
+                                            <th scope="col" class="text-right">Price</th>
+                                        </tr>
+                                        <?php foreach ($currentID as $product) : ?>
+                                <!---->
+                                        <tr>
+                                            <td><a href="#"><?php echo $product['productName']; ?></a></td>
+                                            <td class="text-right">$<?php echo $product['listPrice']; ?></td>
+                                        </tr>
+                                        <?php endforeach; ?>
+                                <!---->      
+                                    </table>       
+                                <?php endforeach; ?>
+                                <!---->
+                                </div>      
+                            </div>
+                        </div>
                     </div>
-                </div>
+                    <div class="col-sm"></div>
             </div>
-        </div>
-        <div class="col-sm"></div>
-</div>
-                    <!-- Footer -->
+                    
                     <?php include("../includes/footer.html") ?>
