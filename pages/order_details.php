b298078b784d73b809a25d505537f4d945af6cb1
Justin Mangan <jm0015052@otc.edu>
2018-03-08 11:26:13 -0600

    Working on problems with queries

--- pages/order_details.php
+++ pages/order_details.php
@@ -6,7 +6,7 @@ require_once('../includes/init.php');
 // Connect to the database
 require_once('../includes/db.php');
 
-// get catagory ID***CHANGE PARAMS
+// get category ID
 $order_ID = filter_input(INPUT_GET, 'order_ID', FILTER_VALIDATE_INT);
 if ($order_ID == NULL || $order_ID == FALSE) {
     $order_ID = 1;
@@ -17,13 +17,34 @@ $current = "orderDetails";
 $title = "Guitars- order details";
 
 // get all products
-$orderItems = getMany("SELECT * FROM `orderItems` JOIN `products` ON `orderItems`.`productID` = `products`.`productID` WHERE `orderItems`.`orderID` = $order_ID", [], $conn);
-$details = getMany("SELECT * FROM `orders` JOIN `orderItems` ON `orders`.`orderID` = `orderItems`.`orderID` WHERE `order`.`orderID` = $order_ID", [], $conn);
-$customer = getOne("SELECT * FROM `orders` JOIN `customers` ON `orders`.`customerID` = `customers`.`customerID` WHERE `order`.`orderID` = $order_ID", [], $conn);
+// $orderItems = getMany("SELECT * FROM `orderItems` JOIN `products` ON `orderItems`.`productID` = `products`.`productID` WHERE `orderItems`.`orderID` = $order_ID", [], $conn);
 
-$customer_ID = $customer['customerID'];
+// $details = getMany("SELECT * FROM `orders` JOIN `orderItems` ON `orders`.`orderID` = `orderItems`.`orderID` WHERE `order`.`orderID` = $order_ID", [], $conn);
 
-$address = getOne("SELECT * FROM `customers` JOIN `addresses` ON `customers`.`customerID` = `addresses`.`customerID` AND `customer`.`billingAddressID` = `addresses`.`addressID` WHERE `customer`.`customerID` = $customer_ID", [], $conn);
+// $customer = getOne("SELECT * FROM `orders` JOIN `customers` ON `orders`.`customerID` = `customers`.`customerID` WHERE `order`.`orderID` = $order_ID", [], $conn);
+
+// $customer_ID = $customer['customerID'];
+
+// $address = getOne("SELECT * FROM `customers` JOIN `addresses` ON `customers`.`customerID` = `addresses`.`customerID` AND `customer`.`billingAddressID` = `addresses`.`addressID` WHERE `customer`.`customerID` = $customer_ID", [], $conn);
+
+
+
+$orderItems = getMany("SELECT * FROM `orders`
+                JOIN `customers` ON `orders`.`customerID` = `customers`.`customerID`
+                JOIN `orderItems` ON `orders`.`orderID` = `orderItems`.`orderID`
+                JOIN `products` ON `orderItems`.`productID` = `products`.`productID`
+                JOIN `addresses` ON `customers`.`billingAddressID` = `addresses`.`addressID`
+                WHERE `orders`.`orderID` = $order_ID", [], $conn);
+
+
+
+
+
+
+
+
+
+
 
 // $item_price = $details['itemPrice'];
 // $item_price_f = "$".number_format($item_price, 2);
@@ -35,16 +56,8 @@ $address = getOne("SELECT * FROM `customers` JOIN `addresses` ON `customers`.`cu
 // $shipping_amount_f = "$".number_format($shipping_amount, 2);
 
 
-
-//$billing_address = $address['addressID'];
-
-// $queryAllUnshipped = 'SELECT * FROM orders WHERE shipDate is null ORDER BY orderID';
-// $statement1 = $conn->prepare($queryAllUnshipped);
-// $statement1->execute();
-// $unshipped = $statement1->fetchAll();
-// $statement1->closeCursor();
-
 var_dump($orderItems);
+var_dump($order_ID);
 ?>
 
 
